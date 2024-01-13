8281278aa24f116a556322b29e6b580ade7dc053
mvayk <pressesc@outlook.com>
2024-01-13 22:40:05 +1100

    new file:   init.lua 	new file:   lazy-lock.json 	new file:   lua/main/binds.lua 	new file:   lua/main/init.lua 	new file:   lua/main/lazy.lua 	new file:   lua/main/set.lua 	new file:   plugin/colorschemes.lua 	new file:   plugin/lualine.lua 	new file:   plugin/mason.lua 	new file:   plugin/nvim-cmp.lua 	new file:   plugin/nvim-lint.lua 	new file:   plugin/telescope.lua

---
+++ lua/main/lazy.lua
@@ -0,0 +1,32 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {'folke/tokyonight.nvim', lazy = false},
+    {'nvim-lualine/lualine.nvim', dependencies = {'nvim-tree/nvim-web-devicons'}},
+    {'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = {'nvim-lua/plenary.nvim'}},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+    {'neovim/nvim-lspconfig'},
+    {'mfussenegger/nvim-lint'},
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-cmdline'},
+    {'hrsh7th/cmp-vsnip'},
+    {'hrsh7th/vim-vsnip'},
+    {'L3MON4D3/LuaSnip'},
+    {'saadparwaiz1/cmp_luasnip'},
+})
